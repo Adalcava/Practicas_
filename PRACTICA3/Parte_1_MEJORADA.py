3692596539b4443929a3caef13fd6faf4d016413
Adalcava <cabrera.vazquez.adalberto@gmail.com>
2024-03-10 17:53:00 -0600

    Agregando parte 1 mejorada

--- PRACTICA3/Parte_1_MEJORADA.py
+++ PRACTICA3/Parte_1_MEJORADA.py
@@ -51,42 +51,42 @@ class Computadora(metaclass=ABCMeta):
     @gpu.setter
     def gpu(self, nuevogpu:str):
         self.__gpu=nuevogpu
-            
-    def presentar(self):
-       print(f', su memoria es de {self.__memoria} GB, su procesador es un {self.__procesador}, tiene un almacenamiento de {self.__almacenamiento}, y una gpu {self.__gpu}.')
-        
+       
 class ComputadoraPortatil(Computadora):
     def __init__(self, memoria, procesador,almacenamiento, gpu, marca:str) -> None:
         super().__init__(memoria, procesador, almacenamiento, gpu)
         self.__marca = marca
     
-        
-    def __str__(self)->str:
-        return f'Hola la computadora portatil es de la marca {self.__marca}'
+    @property
+    def marca(self):
+        return self.__marca
 
 class ComputadoraEscritorio(Computadora):
     def __init__(self, memoria, procesador, almacenamiento, gpu, tamaño:str) ->None:
         super().__init__(memoria, procesador, almacenamiento, gpu)
         self.__tamaño = tamaño
         
-    def __str__(self)-> str:
-        return f'Hola la computadora de escritorio tiene un tamaño de {self.__tamaño} cms cúbicos'
+    @property
+    def tamaño(self):
+        return self.__tamaño
     
 class TelefonoInteligente(Computadora):
     def __init__(self, memoria, procesador, almacenaminto, gpu, numero:str) ->None:
         super().__init__(memoria, procesador, almacenaminto, gpu)
         self.__numero= numero
     
-    def __str__(self)-> str:
-        return f'Hola el numero del telefono inteligente es {self.__numero}'
+    @property
+    def numero(self):
+        return self.__numero
 
 class Tablet(Computadora):
     def __init__(self,memoria, procesador, almacenamiento, gpu, peso:str) ->None:
         super().__init__(memoria,procesador, almacenamiento, gpu)
         self.__peso= peso
     
-    def __str__(self)->str:
-         return f'Hola el peso es de {self.__peso} gr'
+    @property
+    def peso(self):
+        return self.__peso
             
 portatil_1=ComputadoraPortatil('16', 'Intel® Celeron® G','1T', 'Zotac(20)','Samsung')
 portatil_2=ComputadoraPortatil('8', 'AMD Ryzen™ 9', '256 Gb', 'SAPPHIRE(11)', 'Apple')
@@ -100,36 +100,44 @@ telefono_1=TelefonoInteligente('8', 'Qualcomm Snapdragon 8 Gen 2', '512 Gb', 'Ad
 telefono_2=TelefonoInteligente('4','Apple A16 Bionic', '256 Gb', 'Apple M2 10-Core GPU', '9981007887')
 telefono_3=TelefonoInteligente('8' ,'Samsung Exynos 2200', '64 Gb', 'Mali-G615 MP6','9981000105')
 
-tablet_1=Tablet('32',' 1.5GHz	Dual-Core Cortex-A9, 1.6GHz', '512 Gb','ARM Mali-T764','500')
+tablet_1=Tablet('32',' 1.5GHz Dual-Core Cortex-A9, 1.6GHz', '512 Gb','ARM Mali-T764','500')
 tablet_2=Tablet('16','Octa-Core Cortex-A7, 1.6GHz','256 Gb','ARM Mali-450MP6', '460')
 tablet_3=Tablet('8','Quad-Core 64Bit Cortex-A53, 2.0GHz','256 Gb','Mali400MP2', '520')
 
-print('Portatil 1:\n'+ str(portatil_1), end='')
-portatil_1.presentar()
-print('\nPortatil 2:\n'+ str(portatil_2), end="")
-portatil_2.presentar()
-print('\nPortatil 3:\n'+ str(portatil_3), end="")
-portatil_3.presentar()
-
-print('\nEscritorio 1:\n'+ str(escritorio_1), end="")
-escritorio_1.presentar()
-print('\nEscritorio 2:\n'+ str(escritorio_2), end="")
-escritorio_2.presentar()
-print('\nEscritorio 3:\n'+ str(escritorio_3), end="")
-escritorio_3.presentar()
-
-print('\nTelefono 1:\n'+ str(telefono_1), end="")
-telefono_1.presentar()
-print('\nTelefono 2:\n'+ str(telefono_2), end="")
-telefono_2.presentar()
-print('\nTelefono 3:\n'+ str(telefono_3), end="")
-telefono_3.presentar()
-
-print('\nTablet 1:\n'+ str(tablet_1), end="")
-tablet_1.presentar()
-print('\nTablet 2:\n'+ str(tablet_2), end="")
-tablet_2.presentar()
-print('\nTablet 3:\n'+ str(tablet_3), end="")
-tablet_3.presentar()
 
 
+data_portatil=[
+    ["Dispositivo",'Memoria [Gb]', 'Procesador', 'Almacenamiento','GPU', 'Marca'],
+    ['Portatil 1', portatil_1.memoria, portatil_1.procesador, portatil_1.almacenamiento, portatil_1.gpu, portatil_1.marca],
+    ['Portatil 2', portatil_2.memoria, portatil_2.procesador, portatil_2.almacenamiento, portatil_2.gpu, portatil_2.marca],
+    ['Portatil 3', portatil_3.memoria, portatil_3.procesador, portatil_3.almacenamiento, portatil_3.gpu, portatil_3.marca]
+]
+
+data_escritorio=[
+    ["Dispositivo",'Memoria [Gb]', 'Procesador', 'Almacenamiento','GPU', 'Tamaño [cm3]'],
+    ['Escritorio 1', escritorio_1.memoria, escritorio_1.procesador, escritorio_1.almacenamiento, escritorio_1.gpu, escritorio_1.tamaño],
+    ['Escritorio 2', escritorio_2.memoria, escritorio_2.procesador, escritorio_2.almacenamiento, escritorio_2.gpu, escritorio_2.tamaño],
+    ['Escritorio 3', escritorio_3.memoria, escritorio_3.procesador, escritorio_3.almacenamiento, escritorio_3.gpu, escritorio_3.tamaño]
+]
+
+data_telefono=[
+    ["Dispositivo",'Memoria [Gb]', 'Procesador', 'Almacenamiento','GPU', 'Numero'],
+    ['Telefono 1', telefono_1.memoria, telefono_1.procesador, telefono_1.almacenamiento, telefono_1.gpu, telefono_1.numero],
+    ['Telefono 2', telefono_2.memoria, telefono_2.procesador, telefono_2.almacenamiento, telefono_2.gpu, telefono_2.numero],
+    ['Telefono 3', telefono_3.memoria, telefono_3.procesador, telefono_3.almacenamiento, telefono_3.gpu, telefono_3.numero]
+]
+
+data_tablet=[
+    ["Dispositivo",'Memoria [Gb]', 'Procesador', 'Almacenamiento','GPU', 'Peso [gr]'],
+    ['Tablet 1', tablet_1.memoria, tablet_1.procesador, tablet_1.almacenamiento, tablet_1.gpu, tablet_1.peso],
+    ['Tablet 2', tablet_2.memoria, tablet_2.procesador, tablet_2.almacenamiento, tablet_2.gpu, tablet_2.peso],
+    ['Tablet 3', tablet_3.memoria, tablet_3.procesador, tablet_3.almacenamiento, tablet_3.gpu, tablet_3.peso]
+]
+
+print(tabulate(data_portatil, headers="firstrow", tablefmt="grid"))
+print('')
+print(tabulate(data_escritorio, headers="firstrow", tablefmt="grid"))
+print('')
+print(tabulate(data_telefono, headers="firstrow", tablefmt="grid"))
+print('')
+print(tabulate(data_tablet, headers="firstrow", tablefmt="grid"))
--- PRACTICA3/pruebas.py
+++ PRACTICA3/pruebas.py
@@ -1,59 +1,13 @@
-class Alumno:
-    
-    def __init__(self, nombre:str, boleta:str, edad:int) -> None:
-        self.__nombre = nombre
-        self.__boleta = boleta
-        self.__edad = edad
-    
-    @property
-    def boleta(self):
-        return self.__boleta
-    
-    @boleta.setter
-    def boleta(self, nuevaBoleta:str):
-        self.__boleta = nuevaBoleta   
-    
-    @property
-    def edad(self):
-        return self.__edad
-    
-    @edad.setter
-    def edad(self, edad:int):
-        self.__edad = edad
-    
-    @property 
-    def nombre(self):
-        return self.__nombre
-    
-    @nombre.setter
-    def nombre(self, nuevoNombre:str):
-        if(len(nuevoNombre) != 0):
-            self.__nombre = nuevoNombre
-          
-    
-        
-alumno1 = Alumno("Jose Luis", "202456779", 25)
+from abc import ABC, abstractmethod
 
-alumno1.nombre = "asas"
-print(alumno1.edad)
+class MiClaseAbstracta(ABC):
+    @abstractmethod
+    def metodo_abstracto(self):
+        pass
 
-'''class Persona:
-    def __init__(self, nombre:str, edad:int)->None:
-        self.__nombre=nombre
-        self.__edad= edad
-    
-    def presentar(self):
-        print(f'Hola mi nombre es{self.__nombre} y tengo {self.__edad} años')
-        
-class Alumno(Persona):
-    def __init__(self, nombre:str, edad:int ,peso:str)->None:
-        super().__init__(nombre,edad)
-        self.__peso= peso
+class MiClaseConcreta(MiClaseAbstracta):
+    def metodo_abstracto(self):
+        print("Implementación del método abstracto")
 
-    def __str__(self)->str:
-        return f'Hola mi nombre es {self.__peso}'
-    
-    
-alumno1=Alumno("Jose luis", 20, '12')
-alumno1.presentar()
-print(alumno1)'''
+print(issubclass(MiClaseAbstracta, ABC))  # True
+print(issubclass(MiClaseConcreta, ABC))   # True
